efcacee173794e7efb0d2bf121497d2dd8cdfe1f
Jacob Juarez <jjuare4@lsu.edu>
2019-02-20 22:55:31 +0000

    Whistle is set. Well almost. Just press z too shoot a projectile, but it still needs some adjusting

---
+++ Assets/PlayerAbilities.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAbilities : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject whistlePrefab;
+    bool fired = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+
+    }
+
+    void Shoot()
+    {
+        //whistle
+        Instantiate(whistlePrefab, firePoint.position, firePoint.rotation);
+    }
+}
