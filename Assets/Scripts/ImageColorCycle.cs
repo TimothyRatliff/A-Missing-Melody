edd355b5c7a43f7bacced51f7dd64ad67a9b41d6
Mohamed Jabbar <moejay5@yahoo.com>
2019-02-27 23:17:42 +0000

    Main Menu & TextMesh Pro

---
+++ Assets/Scripts/ImageColorCycle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageColorCycle : MonoBehaviour
+{
+    private Color lerpedColor = Color.white;
+    private Image image;
+    public Color white = new Color(1, 1, 1, .96f);
+    public Color green = new Color(0, 1, 0, .96f);
+    public int slowIndex = 6;
+
+    void Start()
+    {
+        image = transform.GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        lerpedColor = Color.Lerp(white, green, Mathf.PingPong(Time.time/slowIndex, 1));
+        image.color = lerpedColor;
+    }
+}
---
+++ Assets/Scripts/TextColorCycle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextColorCycle : MonoBehaviour
+{
+    private Color lerpedColor = Color.white;
+    private Text text;
+    public Color color1 = new Color(0.5f, 0.5f, 0.5f, 1);
+    public Color color2 = new Color(1, 1, 1, 1);
+    public int slowIndex = 2;
+
+    void Start()
+    {
+        text = transform.GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        lerpedColor = Color.Lerp(color1, color2, Mathf.PingPong(Time.time/slowIndex, 1));
+        text.color = lerpedColor;
+    }
+}
