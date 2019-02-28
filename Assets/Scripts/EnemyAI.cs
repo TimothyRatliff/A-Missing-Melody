8225ee03849beec60bb317719ea8a65edd419451
Jacob Juarez <jjuare4@lsu.edu>
2019-02-28 01:45:54 +0000

    Shitty Grunt follow

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public LayerMask enemyMask;
+    public float speed;
+    private Transform target;
+    Rigidbody2D myBody;
+    Transform myTrans;
+
+    void Start()
+    {
+        myTrans = this.transform;
+        myBody = this.GetComponent<Rigidbody2D>();
+        SpriteRenderer mySprite = this.GetComponent<SpriteRenderer>();
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    void FixedUpdate()
+    {
+        if (myTrans.position.x < target.position.x)
+        {
+            Vector3 currentRotation = myTrans.eulerAngles;
+            currentRotation.y = 0;
+            myTrans.eulerAngles = currentRotation;
+            myTrans.position = Vector2.MoveTowards(myTrans.position, target.position, speed * Time.deltaTime);
+        }
+        else
+        {
+            Vector3 currentRotation = myTrans.eulerAngles;
+            currentRotation.y = 180;
+            myTrans.eulerAngles = currentRotation;
+            Debug.Log(myTrans.eulerAngles);
+            myTrans.position = Vector2.MoveTowards(myTrans.position, target.position, speed * Time.deltaTime);
+        }
+    }
+
+}
