28525b7959875c0a0e2baae82257720e3120e8db
Mohamed Jabbar <moejay5@yahoo.com>
2019-03-13 22:16:34 +0000

    Enemy AI: Attack & Damage

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 public class EnemyAI : MonoBehaviour
 {
     public float speed;
+    private float defaultSpeed;
+    public float headHight = 1f;
     private Transform target;
-    public bool shot;
+    public bool stunned, attacking;
     public float stunTime = 3;
-    public int health = 10;
+    private float defaultStunTime;
+    public float attackCooldown = 3;
+    private float defaultAttackCooldown;
+
+    public int health = 1;
     Rigidbody2D myBody;
     Transform myTrans;
     public GameObject itemToSpawn;
 
+    private bool playerInAttack = false;
+
     void Start()
     {
+        defaultSpeed = speed;
+        defaultStunTime = stunTime;
+        defaultAttackCooldown = attackCooldown;
         myTrans = this.transform;
         myBody = this.GetComponent<Rigidbody2D>();
         SpriteRenderer mySprite = this.GetComponent<SpriteRenderer>();
@@ -25,39 +36,50 @@ public class EnemyAI : MonoBehaviour
 
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
 
+        stunned = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerAbilities>().fired;
+
+        //stun//
+
+        if (stunned)
         {
-            shot = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerAbilities>().fired;
-
-            //stun//
-
-            if (shot.Equals(true))
+            stunTime -= Time.deltaTime;
+            speed = 0;
+            if (stunTime < 0)
             {
-                stunTime -= Time.deltaTime;
-                speed = 0;
-                if (stunTime < 0)
-                {
-                    shot = false;
-                    GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerAbilities>().fired = false;
-                    speed = 3;
-                    stunTime = 3;
-                }
-
+                stunned = false;
+                GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerAbilities>().fired = false;
+                speed = defaultSpeed;
+                stunTime = defaultStunTime;
             }
-        }
 
-        if (myTrans.position.x < target.position.x)
-        {
-            Vector3 currentRotation = myTrans.eulerAngles;
-            currentRotation.y = 0;
-            myTrans.eulerAngles = currentRotation;
-            myTrans.position = Vector2.MoveTowards(myTrans.position, target.position, speed * Time.deltaTime);
-        }
-        else
-        {
-            Vector3 currentRotation = myTrans.eulerAngles;
-            currentRotation.y = 180;
-            myTrans.eulerAngles = currentRotation;
-            myTrans.position = Vector2.MoveTowards(myTrans.position, target.position, speed * Time.deltaTime);
+        } else {
+            if (myTrans.position.x < target.position.x)
+            {
+                Vector3 currentRotation = myTrans.eulerAngles;
+                currentRotation.y = 0;
+                myTrans.eulerAngles = currentRotation;
+                myTrans.position = Vector2.MoveTowards(myTrans.position, target.position, speed * Time.deltaTime);
+            }
+            else
+            {
+                Vector3 currentRotation = myTrans.eulerAngles;
+                currentRotation.y = 180;
+                myTrans.eulerAngles = currentRotation;
+                myTrans.position = Vector2.MoveTowards(myTrans.position, target.position, speed * Time.deltaTime);
+            }
+            if (attacking)
+            {
+                //TODO: put attack animation here
+                Debug.Log("Enemy Attacking (need animation)");
+                attackCooldown -= Time.deltaTime;
+                speed = 0;
+                if (attackCooldown < 0)
+                {
+                    attacking = false;
+                    speed = defaultSpeed;
+                    attackCooldown = defaultAttackCooldown;
+                }
+            }
         }
 
         if (health <= 0)
@@ -68,9 +90,11 @@ public class EnemyAI : MonoBehaviour
 
     private void OnDisable()
     {
+        //Item Spawner//
         SpawnItem(itemToSpawn);
     }
 
+    // Spawns GameObject upwards
     private void SpawnItem(GameObject item)
     {
         if (item != null)
@@ -78,4 +102,46 @@ public class EnemyAI : MonoBehaviour
             Instantiate(item, new Vector3(transform.position.x, transform.position.y, 0), transform.rotation);
         }
     }
+    
+    // get the direction of the collision
+    void OnCollisionEnter2D(Collision2D obj)
+     {  
+        if (obj.gameObject.tag == "Player")
+        {
+            Vector3 direction = transform.position - obj.gameObject.transform.position;
+            if (Mathf.Abs (direction.x) < Mathf.Abs (direction.y) && stunned) {
+                if(Mathf.Abs (direction.y) > headHight && Mathf.Abs (direction.x) < headHight/2)
+                {
+                    health--;
+                }
+            }
+        }
+     }
+
+     void OnTriggerEnter2D(Collider2D obj)
+     {  
+        if (obj.gameObject.tag == "Player")
+        {
+            playerInAttack = true;
+        }
+     }
+
+    void OnTriggerStay2D(Collider2D obj)
+     {  
+        if (obj.gameObject.tag == "Player")
+        {
+            if (!stunned)
+            {
+                attacking = true;
+            }
+        }
+     }
+
+     void OnTriggerExit2D(Collider2D obj)
+     {  
+        if (obj.gameObject.tag == "Player")
+        {
+            playerInAttack = false;
+        }
+     }
 }
