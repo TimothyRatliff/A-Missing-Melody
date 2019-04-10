9f9261afdee5d2a46bad9c37b805c9cc39bf6eb6
Timothy Ratliff <timothykratliff@gmail.com>
2019-04-10 15:58:29 +0000

    remove MusicManager from several scenes

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -9,9 +9,6 @@ public class MusicManager : MonoBehaviour
     // public GameObject gameMenuManager;
     private static MusicManager _instance;
     public AudioSource ost;
-    public string[] level1;
-    public string[] level2;
-    public string[] MainMenu;
     public int currentLevel = 1;
 
     public static MusicManager instance
@@ -46,6 +43,7 @@ public class MusicManager : MonoBehaviour
             if(this != _instance)
                 Destroy(this.gameObject);
         }
+        ost.Play();
         
         
     }
@@ -61,13 +59,8 @@ public class MusicManager : MonoBehaviour
         Debug.Log("game is paused, applying music fx");
     }
 
-    // void Update()
-    // {
-    //     level1 = new string[7]{"Level 1-1", "Level 1-2", "Level 1-3", "Level 1-4", "Level 1-5", "Level 1-6", "Level 1-7"};
-    //     Scene currentScene = SceneManager.GetActiveScene();
-    //     if(level1.Contains(currentScene.name))
-    //     {
-    //         currentLevel = 1;
-    //     }
-    // }
+    void Update()
+    {
+        
+    }
 }
