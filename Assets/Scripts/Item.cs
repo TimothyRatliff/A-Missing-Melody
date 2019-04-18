30b4383aa934a84aeecfc4356f58f1bf8f6c2001
Mohamed Jabbar <moejabbary5@gmail.com>
2019-04-18 17:53:41 +0000

    Fix Item Spawn + Wall is Climbable only after pickup

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -7,9 +7,11 @@ public class Item : MonoBehaviour
     private Rigidbody2D rb2D;
     public float thrust = 10.0f;
     public float turnMult = 2;
+    private GameObject wallBlock;
 
     void Start()
     {
+        wallBlock = GameObject.Find("Wall Block"); 
         rb2D = gameObject.GetComponent<Rigidbody2D>();
         rb2D.AddForce(transform.up * thrust);
     }
@@ -18,5 +20,15 @@ public class Item : MonoBehaviour
     void Update()
     {
         transform.Rotate(0, Time.deltaTime * turnMult, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            if (wallBlock != null)
+                wallBlock.SetActive(false);
+            transform.gameObject.SetActive(false);
+        }
     }
 }
