d0c20182759a6510ed5f03aed73f1d65e741f436
Jacob Juarez <jjuare4@lsu.edu>
2019-04-23 01:31:44 +0000

    Trumpet animation

--- Assets/Scripts/PlayerAbilities.cs
+++ Assets/Scripts/PlayerAbilities.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class PlayerAbilities : MonoBehaviour
 {
     public GameObject whistlePrefab;
+    private Animator animWhistle;
     private Animator anim;
     public bool whistleFired = false;
     public AudioSource whistle;
     public GameObject trumpetProjectile;
     private Quaternion rot;
     private Vector3 pos;
-    private float cooldown;
 
     void Awake()
     {
-        anim = whistlePrefab.GetComponent<Animator>();
+        animWhistle = whistlePrefab.GetComponent<Animator>();
+        anim = transform.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,45 +25,32 @@ public class PlayerAbilities : MonoBehaviour
         //Whistle or trumpet
         if (Input.GetButtonDown("Fire1"))
         {
-            if (cooldown < 0)
+            if (PlayerPrefs.GetInt("trumpet", 0) == 1)
             {
-                if (PlayerPrefs.GetInt("trumpet", 0) == 1)
+                if (transform.lossyScale.x == 1)
                 {
-                    StartCoroutine(shoot());
-                    cooldown = 0.6f;
+                    rot = Quaternion.Euler(0, 180, 0);
+                    pos = transform.position + -(Vector3.right * 2) + (Vector3.up * 1.5f);
                 }
                 else
                 {
-                    cooldown = 1f;
-                    anim.SetTrigger("usingWhistle");
-                    whistleFired = true;
-                    whistle.Play();
+                    rot = Quaternion.Euler(0, 0, 0);
+                    pos = transform.position + (Vector3.right * 2) + (Vector3.up * 1.5f);
                 }
+                //Add Trumpet triggers animations
+                anim.SetTrigger("isTrumpeting");
+                GameObject.Instantiate(trumpetProjectile, pos, rot);
+            }
+            else
+            {
+                animWhistle.SetTrigger("usingWhistle");
+                whistleFired = true;
+                whistle.Play();
             }
         }
         else
         {
            whistleFired = false; 
-           cooldown -= Time.deltaTime;
         }
-    }
-
-    private IEnumerator shoot()
-    {
-        if (transform.lossyScale.x == 1)
-        {
-            rot = Quaternion.Euler(0, 180, 0);
-            pos = transform.position + -(Vector3.right * 2) + (Vector3.up * 1.5f);
-        }
-        else
-        {
-            rot = Quaternion.Euler(0, 0, 0);
-            pos = transform.position + (Vector3.right * 2) + (Vector3.up * 1.5f);
-        }
-        //Add Trumpet triggers animations
-        anim.SetTrigger("isTrumpeting");
-
-        yield return new WaitForSeconds(0.2f);
-        GameObject.Instantiate(trumpetProjectile, pos, rot);
     }
 }
